020b948401ad4b573eec67b7d38ac2efdc2acd30
David Govea <govea.d@gmail.com>
2019-01-25 00:53:57 -0800

    hardcode default config

--- util/configHandler.js
+++ util/configHandler.js
@@ -2,9 +2,9 @@ import resolveConfig from 'tailwindcss/resolveConfig';
 
 let file = require('tailwindcss/stubs/simpleConfig.stub');
 
-try {
-    file = require('../../../tailwind.config');
-} catch (e) {}
+// try {
+//     file = require('../../../tailwind.config');
+// } catch (e) {}
 
 const {theme} = resolveConfig(file);
 
